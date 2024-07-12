21a6d90cb6f3330d6a197e2518348152cc0e3c64
nguyenquang2002 <nguyenquang22062002@gmail.com>
2024-07-12 21:04:12 +0700

    Movement and Show Kick Button

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] float RotationSpeed;
+    [SerializeField] Camera cam;
+    [SerializeField] Button kickButton;
+    [SerializeField] float checkRadius = 0.7f;
+
+    private float x, z;
+
+    Rigidbody rb;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Movement();
+        ShowKickButton();
+    }
+
+    void GetInput()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+    }
+    void Movement()
+    {
+        Vector3 targetVector = new Vector3(x, 0, z);
+        Vector3 movementVector = MoveTowardTarget(targetVector);
+        RotateTowardMovementVector(movementVector);
+        animator.SetBool("Running", x != 0 || z != 0);
+    }
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        float speedMove = speed * Time.deltaTime;
+        
+
+        targetVector = Quaternion.Euler(0, cam.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        Vector3 targetPosition = transform.position + targetVector * speedMove;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+    private void RotateTowardMovementVector(Vector3 movementDirection)
+    {
+        if (movementDirection.magnitude == 0) 
+        { 
+            return; 
+        }
+        var rotation = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed);
+    }
+    private void ShowKickButton()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, checkRadius);
+        bool ballNearby = false;
+        foreach(Collider collider in hitColliders)
+        {
+            if (collider.CompareTag("Ball"))
+            {
+                ballNearby = true;
+                break;
+            }
+        }
+        kickButton.gameObject.SetActive(ballNearby);
+    }
+}
